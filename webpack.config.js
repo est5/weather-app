072c6a19d3b85ce968deacca8a96812eafa9e1c9
est5 <ghostrekon5500@ya.ru>
2022-03-02 09:59:40 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'eval',
+};
